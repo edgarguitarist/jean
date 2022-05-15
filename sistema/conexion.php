324a44a1247347380f2f71005a9980619cee688f
edgarguitarist <edgarguitarist@gmail.com>
2022-05-15 13:48:21 -0500

    fix: conexion a la base

--- sistema/conexion.php
+++ sistema/conexion.php
@@ -1,6 +1,6 @@
 <?php
-	// $conexion=mysqli_connect("localhost","u103247758_jossyemb",">6tZhb#:4","u103247758_jossyemb");
-	$conexion=mysqli_connect("localhost","root","","u103247758_jossyemb");
+	$conexion=mysqli_connect("localhost","u103247758_jossyemb",">6tZhb#:4","u103247758_jossyemb");
+	//$conexion=mysqli_connect("localhost","root","","u103247758_jossyemb");
 	
 	//if(!$conexion){echo "ERROR AL CONECTAR CON LA BASE DE DATOS";}
 	//else{echo "CONECTADO A LA BASE DE DATOS";}
